81fdffbd96862d0da9885edb47337d89f90ccdc9
Jhkier <Glenn@Broadhurst.net.au>
2021-08-05 02:35:51 +0100

    Add files via upload

--- spray_function.py
+++ spray_function.py
@@ -8,14 +8,19 @@ from board import *
 
 from datetime import date
 
-jet_Nozzle = digitalio.DigitalInOut(D5)
+jet_Nozzle = digitalio.DigitalInOut(D5)    #should change the pin
 jet_Nozzle.direction = digitalio.Direction.OUTPUT
+
+jet_Spray_led = digitalio.DigitalInOut(D6)  #should change the pin too
+jet_Spray_led.direction = digitalio.Direction.OUTPUT
 
 def spray_function():
     while True:
         jet_Nozzle.value = True
+        jet_Spray_led = True
         time.sleep(6)               # how long its on for... make this 5 in future.
         jet_Nozzle.value = False
+        jet_Spray_led = False
         time.sleep(10)              #how long off for - make this 300 for 5 minutes off
 
 spray_function()
