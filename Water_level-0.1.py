23cd61569f7d7348f0f0e1e1cb11fbbe35bbf2e9
Jhkier <Glenn@Broadhurst.net.au>
2021-08-05 04:20:09 +0100

    Add files via upload
    reverse water level from water_high to water_low to make it more logical.

--- Water_level-0.1.py
+++ Water_level-0.1.py
@@ -8,28 +8,28 @@ from board import *
 
 from datetime import date
 
-water_level_led = digitalio.DigitalInOut(D5)
+water_level_led = digitalio.DigitalInOut(D5)                  #PIN TO PUMP
 water_level_led.direction = digitalio.Direction.OUTPUT
 
-water_level_sensor = digitalio.DigitalInOut(D18)
+water_level_sensor = digitalio.DigitalInOut(D18)              #PIN to WATER LEVEL SENSOR
 water_level_sensor.switch_to_input(pull=digitalio.Pull.UP)
 water_level_sensor.pull = digitalio.Pull.UP
 
 def water_level_function():
     current_water_level = None
     while True:
-        water_high = water_level_sensor.value
-        if water_high != current_water_level:
-            water_changed(water_high)
-        current_water_level = water_high
-        time.sleep(60)
+        water_low = water_level_sensor.value
+        if water_low != current_water_level:
+            water_changed(water_low)
+        current_water_level = water_low
+        time.sleep(6)                                # INCREASE TO 60 TO CHECK EVERY MIN
 
-def water_changed(water_high: bool):
-        water_level_led.value = water_high
-        if water_high == True:
-            print("They System has Water")
+def water_changed(water_low: bool):
+        water_level_led.value = water_low
+        if water_low == True:
+            print("The system is dry and needs water")
         else:
-            print("The System is Dry and needs Water")
+            print("The System has water")
 
 
 
